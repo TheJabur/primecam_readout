faf37512ebbb7702576fc7d94cf7387b1afd9d0f
James Burgoyne <thejaburjunk+github@gmail.com>
2023-04-19 13:23:10 -0700

    Added _cfg_board.bak.py and _cfg_queen.bak.py.

---
+++ _cfg_board.bak.py
@@ -0,0 +1,24 @@
+###############################
+## RFSoC board configuration ##
+## which lives on the boards ##
+###############################
+
+## board identifier
+bid = 1    # should match physical id on board
+
+## redis server configuration
+host = 'localhost'
+# host = '192.168.2.80'
+port = 6379
+db = 0
+
+
+#########################
+## DO NOT MODIFY BELOW ##
+#########################
+
+root_dir = ''
+drone_dir = ''
+
+## drone identifier
+drid = 0
---
+++ _cfg_queen.bak.py
@@ -0,0 +1,8 @@
+## Queen configuration file
+## which lives on the control server
+
+## redis server configuration
+host = 'localhost'
+# host = '192.168.2.80'
+port = 6379
+db = 0
