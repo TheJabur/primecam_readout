cd875173b6ebb9c582ea3fec2b5d29e379e1c6b5
James Burgoyne <thejaburjunk+github@gmail.com>
2023-05-05 09:09:58 -0700

    Added __del__ function to TimeStream. Added socket close to it.

--- timestream.py
+++ timestream.py
@@ -25,24 +25,29 @@ class TimeStream:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.host, self.port))
 
+
     def capturePacket(self, buffer_size=9000):
         message, address = self.sock.recvfrom(buffer_size)
         return bytearray(message)
         # return message.decode(), address
 
+
     def captureNpackets(self, N, buffer_size=9000):
         return np.array([self.capturePacket(buffer_size) for _ in range(N)])
     
+
     def byteshiftPackets(self, packets, byteshift=-1):
         return np.array([
             np.roll(p, byteshift) 
             for p in packets])
     
+
     def convertPackets(self, packets):
         return np.array([
             np.frombuffer(p, dtype="<i").astype("float")
             for p in packets])
     
+
     def getTimeStreamChunk(self, N):
         """Grab a chunk of N packets from the timestream.
         Returns I and Q.
@@ -56,10 +61,12 @@ class TimeStream:
         
         return I, Q
 
+
     # def send_message(self, message, address):
     #     self.sock.sendto(message.encode(), address)
 
-    def close(self):
+
+    def __del__(self):
         self.sock.close()
 
 
