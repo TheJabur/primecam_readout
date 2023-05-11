cd7b0907bf6f7f748394ade5aef2ccf42ef7adf5
James Burgoyne <thejaburjunk+github@gmail.com>
2023-05-11 09:55:18 -0700

    Functionally refactored queen_gui

--- queen_gui.py
+++ queen_gui.py
@@ -1,21 +1,19 @@
-########################################################
-### Queen GUI.                                       ###
-### Interfaces with queen.py.                        ###
-###                                                  ###
-### James Burgoyne jburgoyne@phas.ubc.ca             ###
-### CCAT Prime 2023                                  ###
-########################################################
+# ============================================================================ #
+# queen_gui.py
+# Queen GUI: Interfaces with queen.py
+# James Burgoyne jburgoyne@phas.ubc.ca 
+# CCAT Prime 2023  
+# ============================================================================ #
 
 
 
-###############
-### IMPORTS ###
+# ============================================================================ #
+# Imports
+# ============================================================================ #
+
 
 import sys
 import numpy as np
-import math
-import random
-import time
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
@@ -30,66 +28,65 @@ from timestream import TimeStream
 
 
 
-###############
-### THREADS ###
-
-
-class QueenCommandThread(QThread):
-    finished = pyqtSignal(object)
-
-    def __init__(self, parent, com_str, com_args):
-        QThread.__init__(self, parent)
-        self.com_str = com_str
-        self.com_args = com_args
-
-    def run(self):
-        # time.sleep(3)
-        ret = _sendQueenCommand(self.com_str, self.com_args)
-        self.finished.emit((ret, self.com_str, self.com_args))
-
-
-class AlcoveCommandThread(QThread):
-    finished = pyqtSignal(object)
-
-    def __init__(self, parent, com_str, com_to, com_args):
-        QThread.__init__(self, parent)
-        self.com_str = com_str
-        self.com_to = com_to
-        self.com_args = com_args
-
-    def run(self):
-        # time.sleep(3)
-        ret = _sendAlcoveCommand(self.com_str, self.com_to, self.com_args)
-        self.finished.emit((ret, self.com_str, self.com_to, self.com_args))
-
-
-
-###################
-### MAIN WINDOW ###
+# ============================================================================ #
+# Main Window
+# ============================================================================ #
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Window title and icon
-        self.setWindowTitle("CCATpHive Experimental Readout GUI (PyQt)")
+        self.setWindowTitle("CCATpHive Readout Development GUI")
         self.setWindowIcon(QIcon(QPixmap('./gui_assets/icon.png')))
 
-        # Loading gif
+        # store 'loading' gif in memory for common use
         self.movie_loading = QMovie('./gui_assets/loading.gif')
         self.movie_loading.setScaledSize(QSize(10, 10))
 
-
-        # Create the main widget
-        widget = QWidget(self)
+        widget = QWidget(self) # main widget
         self.setCentralWidget(widget)
+        layout = QVBoxLayout(widget) # use a vertical layout
 
-        # Create the main vertical layout
-        layout = QVBoxLayout(widget)
+        # setup UI elements
+        self.uisetupQueenListen(layout) # Queen listen button
+        self.uisetupTimestream(layout) # Time stream figure
+        self.uisetupAlcoveCommand(layout) # Alcove command
+        self.uisetupConsole(layout) # Console
+    
+
+    def closeEvent(self, event):
+        """Standard function that runs when app is being closed.
+        """
+
+        # stop the queen listening thread
+        if self.queenlisten_thread is not None:
+            self.queenlisten_thread.stop()
+        
+        # ask user for exit confirmation
+        event.ignore()
+        if self.confirmClose():
+            event.accept()
 
 
-        # Queen listen button
+    def confirmClose(self):
+        """Dialogue prompt to confirm app exit.
+        """
+
+        result = QMessageBox.question(self,
+                      "Confirm Exit...",
+                      "Are you sure you want to exit ?",
+                      QMessageBox.Yes| QMessageBox.No)
+        
+        return result == QMessageBox.Yes
+
+
+
+# ============================================================================ #
+# Interface: Queen Listen
+
+
+    def uisetupQueenListen(self, layout):
         self.button_queenlisten = QPushButton('START Queen Listening', self)
         self.button_queenlisten.setCheckable(True)
         self.button_queenlisten.clicked.connect(self.onClickButtonQueenlisten)
@@ -98,7 +95,51 @@ class MainWindow(QMainWindow):
         self.button_queenlisten.click() # auto start queen listen
 
 
-        # Time stream figure
+    def onClickButtonQueenlisten(self):
+        if self.button_queenlisten.isChecked():
+            print("Starting Queen listen mode...")
+            self.updateQueenListenUI(running=True)
+            self.sendQueenListenCommand()
+            
+        else:
+            print("Stopping Queen listen mode...")
+            self.updateQueenListenUI(running=False)
+            if self.queenlisten_thread is not None:
+                self.queenlisten_thread.stop()
+
+
+    def updateQueenListenUI(self, running):
+        if running:
+            self.button_queenlisten.setText('STOP Queen listening')
+
+        else:
+            self.button_queenlisten.setText('START Queen listening')
+            self.button_queenlisten.setChecked(False)
+
+
+    def sendQueenListenCommand(self):
+        self.queenlisten_thread = QueenCommandThread(self, com_str='listenMode', com_args='')
+        self.queenlisten_thread.finished.connect(self.onFinishQueenlisten)
+        self.queenlisten_thread.start()
+
+
+    def onFinishQueenlisten(self, ret):
+        try:
+            thread, com_str, com_args = ret
+            self.queenlisten_thread = thread
+            running = thread is not None
+        except:
+            print("onFinishQueenlisten exception")
+            running = False
+        self.updateQueenListenUI(running)
+    
+
+
+# ============================================================================ #
+# Interface: Timestream 
+
+
+    def uisetupTimestream(self, layout):
         self.figure_timestream = plt.figure(figsize=(5, 3), dpi=100)
         self.canvas_timestream = FigureCanvas(self.figure_timestream)
         layout.addWidget(self.canvas_timestream)
@@ -147,61 +188,6 @@ class MainWindow(QMainWindow):
         self.data_timestream_save = None
 
 
-        # Alcove command
-        layout_alcovecoms = QHBoxLayout()
-        layout.addLayout(layout_alcovecoms)
-
-        self.pulldown_alcovecoms = QComboBox()
-        self.pulldown_alcovecoms.addItems(_comsListAlcove())
-        layout_alcovecoms.addWidget(self.pulldown_alcovecoms)
-
-        self.textbox_alcovecoms_to = QLineEdit()
-        self.textbox_alcovecoms_to.setPlaceholderText("board.drone")
-        layout_alcovecoms.addWidget(self.textbox_alcovecoms_to)
-
-        self.textbox_alcovecoms_args = QLineEdit()
-        self.textbox_alcovecoms_args.setPlaceholderText("args")
-        layout_alcovecoms.addWidget(self.textbox_alcovecoms_args)
-
-        self.button_alcovecoms = QPushButton("Send Alcove Command")
-        self.button_alcovecoms.clicked.connect(self.onClickButtonAlcovecoms)
-        layout_alcovecoms.addWidget(self.button_alcovecoms)
-
-        self.label_alcovecoms = QLabel("")
-        layout_alcovecoms.addWidget(self.label_alcovecoms)
-
-
-        # Console
-        self.console = QPlainTextEdit()
-        layout.addWidget(self.console)
-        self.emitter_console = ConsoleEmitter()
-        self.emitter_console.text_written.connect(self.console.insertPlainText)
-        sys.stdout.write = self.addConsoleMessage
-        sys.stderr.write = self.addConsoleMessage
-
-
-
-    def onClickButtonAlcovecoms(self):
-        com_str = self.pulldown_alcovecoms.currentText()
-        com_to = self.textbox_alcovecoms_to.text()
-        com_args = self.textbox_alcovecoms_args.text()
-        self.updateAlcoveComsUI(True, com_str, com_to, com_args)
-        self.sendAlcoveCommand(com_str, com_to, com_args)
-
-
-    def onClickButtonQueenlisten(self):
-        if self.button_queenlisten.isChecked():
-            print("Starting Queen listen mode...")
-            self.updateQueenListenUI(running=True)
-            self.sendQueenListenCommand()
-            
-        else:
-            print("Stopping Queen listen mode...")
-            self.updateQueenListenUI(running=False)
-            if self.queenlisten_thread is not None:
-                self.queenlisten_thread.stop()
-
-
     def onClickedButtonTimestream(self):
         if self.button_timestream.isChecked():
             try:
@@ -220,6 +206,17 @@ class MainWindow(QMainWindow):
 
             self.data_timestream_save = None
             self.button_timestream_save.setChecked(False)
+
+
+    def updateTimeStreamUI(self, running):
+        if running:
+            self.button_timestream.setText('Stop Time Stream')
+            self.button_timestream.setChecked(True)
+            self.button_timestream_save.setEnabled(True)
+        else:
+            self.button_timestream.setText('Start Time Stream')
+            self.button_timestream.setChecked(False)
+            self.button_timestream_save.setEnabled(False)
 
 
     def onClickedButtonTimestreamSave(self):
@@ -243,80 +240,6 @@ class MainWindow(QMainWindow):
                              filename=base_fname, use_timestamp=True)
         print(f"Saving captured timestream to file: {fname}")
         self.data_timestream_save = None
-
-
-    def onFinishAlcoveCommand(self, ret_tuple):
-        ret, com_str, com_to, com_args = ret_tuple
-        print(f"Return: {ret}")
-        self.updateAlcoveComsUI(False, com_str, com_to, com_args)
-
-
-    def onFinishQueenlisten(self, ret):
-        thread, com_str, com_args = ret
-        self.queenlisten_thread = thread
-        self.updateQueenListenUI(thread is not None)
-
-
-    def sendAlcoveCommand(self, com_str, com_to, com_args):
-        # self.updateAlcoveComsUI(True, com_str, com_to, com_args)
-        alcovecom_thread = AlcoveCommandThread(self, com_str, com_to, com_args)
-        alcovecom_thread.finished.connect(self.onFinishAlcoveCommand)
-        alcovecom_thread.start()
-
-
-    # def sendQueenCommand(self, com_str, com_args):
-    #     self.updateQueenComsUI(True, com_str, com_args)
-    #     queencom_thread = QueenCommandThread(self, com_str, com_args)
-    #     queencom_thread.ret.connect(self.onQueenCommandFinished)
-    #     queencom_thread.start()
-
-
-    def sendQueenListenCommand(self):
-        self.queenlisten_thread = QueenCommandThread(self, com_str='listenMode', com_args='')
-        self.queenlisten_thread.finished.connect(self.onFinishQueenlisten)
-        self.queenlisten_thread.start()
-
-
-    def updateAlcoveComsUI(self, busy, com_str, com_to, com_args):
-        if busy: # waiting for command to finish
-            self.button_alcovecoms.setText(com_str)
-            self.button_alcovecoms.setEnabled(False)
-            self.textbox_alcovecoms_to.setEnabled(False)
-            self.textbox_alcovecoms_args.setEnabled(False)
-            self.pulldown_alcovecoms.setEnabled(False)
-            self.label_alcovecoms.setMovie(self.movie_loading)
-            self.movie_loading.start()
-        else: # on finish
-            # print(self)
-            self.movie_loading.stop()
-            self.movie_loading.start() # what?
-            self.movie_loading.stop() # what? why needed?
-            self.label_alcovecoms.setMovie(None)
-            self.pulldown_alcovecoms.setEnabled(True)
-            self.textbox_alcovecoms_to.setEnabled(True)
-            self.textbox_alcovecoms_args.setEnabled(True)
-            self.button_alcovecoms.setEnabled(True)
-            self.button_alcovecoms.setText("Send Alcove Command")   
-
-
-    def updateQueenListenUI(self, running):
-        if running:
-            self.button_queenlisten.setText('STOP Queen listening')
-
-        else:
-            self.button_queenlisten.setText('START Queen listening')
-            self.button_queenlisten.setChecked(False)
-
-
-    def updateTimeStreamUI(self, running):
-        if running:
-            self.button_timestream.setText('Stop Time Stream')
-            self.button_timestream.setChecked(True)
-            self.button_timestream_save.setEnabled(True)
-        else:
-            self.button_timestream.setText('Start Time Stream')
-            self.button_timestream.setChecked(False)
-            self.button_timestream_save.setEnabled(False)
 
 
     def updateTimeStreamSaveUI(self, running):
@@ -385,37 +308,155 @@ class MainWindow(QMainWindow):
         self.canvas_timestream.draw()
 
 
+
+# ============================================================================ #
+# Interface: Alcove Commands 
+
+
+    def uisetupAlcoveCommand(self, layout):
+        """Alcove commands interface.
+        """
+
+        layout_alcovecoms = QHBoxLayout()
+        layout.addLayout(layout_alcovecoms)
+
+        self.pulldown_alcovecoms = QComboBox()
+        self.pulldown_alcovecoms.addItems(_comsListAlcove())
+        layout_alcovecoms.addWidget(self.pulldown_alcovecoms)
+
+        self.textbox_alcovecoms_to = QLineEdit()
+        self.textbox_alcovecoms_to.setPlaceholderText("board.drone")
+        layout_alcovecoms.addWidget(self.textbox_alcovecoms_to)
+
+        self.textbox_alcovecoms_args = QLineEdit()
+        self.textbox_alcovecoms_args.setPlaceholderText("args")
+        layout_alcovecoms.addWidget(self.textbox_alcovecoms_args)
+
+        self.button_alcovecoms = QPushButton("Send Alcove Command")
+        self.button_alcovecoms.clicked.connect(self.onClickButtonAlcovecoms)
+        layout_alcovecoms.addWidget(self.button_alcovecoms)
+
+        self.label_alcovecoms = QLabel("")
+        layout_alcovecoms.addWidget(self.label_alcovecoms)
+
+
+    def onClickButtonAlcovecoms(self):
+        com_str = self.pulldown_alcovecoms.currentText()
+        com_to = self.textbox_alcovecoms_to.text()
+        com_args = self.textbox_alcovecoms_args.text()
+        self.updateAlcoveComsUI(True, com_str, com_to, com_args)
+        self.sendAlcoveCommand(com_str, com_to, com_args)
+
+
+    def updateAlcoveComsUI(self, busy, com_str, com_to, com_args):
+        if busy: # waiting for command to finish
+            self.button_alcovecoms.setText(com_str)
+            self.button_alcovecoms.setEnabled(False)
+            self.textbox_alcovecoms_to.setEnabled(False)
+            self.textbox_alcovecoms_args.setEnabled(False)
+            self.pulldown_alcovecoms.setEnabled(False)
+            self.label_alcovecoms.setMovie(self.movie_loading)
+            self.movie_loading.start()
+        else: # on finish
+            # print(self)
+            self.movie_loading.stop()
+            self.movie_loading.start() # what?
+            self.movie_loading.stop() # what? why needed?
+            self.label_alcovecoms.setMovie(None)
+            self.pulldown_alcovecoms.setEnabled(True)
+            self.textbox_alcovecoms_to.setEnabled(True)
+            self.textbox_alcovecoms_args.setEnabled(True)
+            self.button_alcovecoms.setEnabled(True)
+            self.button_alcovecoms.setText("Send Alcove Command")  
+
+
+    def sendAlcoveCommand(self, com_str, com_to, com_args):
+        # self.updateAlcoveComsUI(True, com_str, com_to, com_args)
+        alcovecom_thread = AlcoveCommandThread(self, com_str, com_to, com_args)
+        alcovecom_thread.finished.connect(self.onFinishAlcoveCommand)
+        alcovecom_thread.start()
+
+
+    def onFinishAlcoveCommand(self, ret_tuple):
+        ret, com_str, com_to, com_args = ret_tuple
+        print(f"Return: {ret}")
+        self.updateAlcoveComsUI(False, com_str, com_to, com_args)
+
+
+    # def sendQueenCommand(self, com_str, com_args):
+    #     self.updateQueenComsUI(True, com_str, com_args)
+    #     queencom_thread = QueenCommandThread(self, com_str, com_args)
+    #     queencom_thread.ret.connect(self.onQueenCommandFinished)
+    #     queencom_thread.start()
+
+
+    
+# ============================================================================ #
+# Interface: Console 
+
+
+    def uisetupConsole(self, layout):
+        """Console interface.
+        """
+
+        self.console = QPlainTextEdit()
+        layout.addWidget(self.console)
+        self.emitter_console = ConsoleEmitter()
+        self.emitter_console.text_written.connect(self.console.insertPlainText)
+        sys.stdout.write = self.addConsoleMessage
+        sys.stderr.write = self.addConsoleMessage
+
+
     def addConsoleMessage(self, text):
+        """Add a message to the console.
+
+        text: (str) The message to add.
+        """
+
         self.emitter_console.text_written.emit(text)
         self.console.moveCursor(QTextCursor.End)
         # self.console.ensureCursorVisible()
-
-
-    def confirmClose(self):
-        result = QMessageBox.question(self,
-                      "Confirm Exit...",
-                      "Are you sure you want to exit ?",
-                      QMessageBox.Yes| QMessageBox.No)
-        
-        return result == QMessageBox.Yes
-    
-
-    def closeEvent(self, event):
-        """Runs when app is being closed."""
-
-        # stop the queen listening thread
-        if self.queenlisten_thread is not None:
-            self.queenlisten_thread.stop()
-        
-        # ask user for exit confirmation
-        event.ignore()
-        if self.confirmClose():
-            event.accept()
     
 
 
-##########################
-### INTERNAL FUNCTIONS ###
+# ============================================================================ #
+# Threading Classes
+# ============================================================================ #
+
+
+class QueenCommandThread(QThread):
+    finished = pyqtSignal(object)
+
+    def __init__(self, parent, com_str, com_args):
+        QThread.__init__(self, parent)
+        self.com_str = com_str
+        self.com_args = com_args
+
+    def run(self):
+        ret = _sendQueenCommand(self.com_str, self.com_args)
+        self.finished.emit((ret, self.com_str, self.com_args))
+
+#region
+class AlcoveCommandThread(QThread):
+    finished = pyqtSignal(object)
+
+    def __init__(self, parent, com_str, com_to, com_args):
+        QThread.__init__(self, parent)
+        self.com_str = com_str
+        self.com_to = com_to
+        self.com_args = com_args
+
+    def run(self):
+        # time.sleep(3)
+        ret = _sendAlcoveCommand(self.com_str, self.com_to, self.com_args)
+        self.finished.emit((ret, self.com_str, self.com_to, self.com_args))
+#endregion
+
+
+
+# ============================================================================ #
+# Internal Functions
+# ============================================================================ #
 
 
 class ConsoleEmitter(QObject):
