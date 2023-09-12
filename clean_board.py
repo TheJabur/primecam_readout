d0ed020a84b92e87c61f71a6d7d712caa4753718
James Burgoyne <thejaburjunk+github@gmail.com>
2023-09-12 09:41:33 -0700

    Added extension filter to clean_board.py plus fixes

--- clean_board.py
+++ clean_board.py
@@ -39,14 +39,17 @@ def main():
 
 # ============================================================================ #
 # cleanDir
-def cleanDir(dname, olderThanDate=None, olderThanDaysAgo=None, largerThanMB=None, confirm=True):
+# TODO: need ability to add (or exclude) file types/exts
+def cleanDir(dname, ftype=None, olderThanDate=None, olderThanDaysAgo=None, largerThanMB=None, confirm=True, testing=True):
     if olderThanDate is None and olderThanDaysAgo is None and largerThanMB is None:
         print("At least one of the filter options is required.")
         return
 
     # warning
     if confirm:
-        print("WARNING: This script will DELETE files!")
+        print(f"WARNING: This script will DELETE files!")
+        if testing:
+            print("TESTING MODE: Will not actually delete.")
         if not _promptConfirm("Continue?"):
             return
 
@@ -57,6 +60,12 @@ def cleanDir(dname, olderThanDate=None, olderThanDaysAgo=None, largerThanMB=None
             for file in files:
                 file_path = os.path.join(root, file)
                 toDel = True
+
+                # extension
+                if ftype:
+                    file_ext = os.path.splitext(file_path)[1]
+                    if file_ext != ftype:
+                        toDel = False
 
                 # date
                 if olderThanDate and toDel:
@@ -85,9 +94,11 @@ def cleanDir(dname, olderThanDate=None, olderThanDaysAgo=None, largerThanMB=None
             for file_path in file_paths_to_del:
 
                 # delete
-                # TODO: commented out for safety during testing
-                # os.remove(file_path) 
-                print(f"Deleted: {file_path}")
+                if not testing:
+                    os.remove(file_path) 
+                    print(f"Deleted: {file_path}")
+                else: 
+                    print(f"NOT deleted (testing): {file_path}")
 
         else:
             print("Cleaning CANCELLED.")
@@ -105,17 +116,25 @@ def cleanTmpDir(**kwargs):
 # ============================================================================ #
 # cleanLogDir
 def cleanLogDir(**kwargs):
-    cleanDir("logs/", **kwargs)
+    cleanDir("logs/", ftype=".log", **kwargs)
 
 
 # ============================================================================ #
 # cleanDroneDirs
-def cleanDroneDirs():
-    print("Functionality not added yet.")
+def cleanDroneDirs(**kwargs):
+    print("cleanDroneDirs functionality not added yet.")
     # cleanDir("tmp/", **kwargs)
     # each drone dir has subdirs which need cleaning
     # but don't touch the files in the base drone dir:
     # drones/drone[1-4]/
+
+
+# ============================================================================ #
+# cleanAll
+def cleanAll(**kwargs):
+    cleanTmpDir(**kwargs)
+    cleanLogDir(**kwargs)
+    cleanDroneDirs(**kwargs)
 
 
 
@@ -147,16 +166,18 @@ def _setupArgparse():
         epilog = "")
 
     # type of cleaning
-    parser.add_argument("type", choices=['all', 'tmp', 'logs', 'drones'], 
+    # choices=['all', 'tmp', 'logs', 'drones']
+    choices=['tmp', 'logs', 'drones']
+    parser.add_argument("type", choices=choices, 
         help="Type of cleaning.")
     
     # TODO: make one of these required
     # limiting options
-    parser.add_argument("-d", "--date", 
-        help="Limit to files older than [date].")
-    parser.add_argument("-t", "--time", 
+    parser.add_argument("-d", "--keep_from_date", 
+        help="Limit to files older than [YYYY-mm-dd].")
+    parser.add_argument("-l", "--keep_last_days", 
         help="Limit to files older than this input, in days.")
-    parser.add_argument("-s", "--size",
+    parser.add_argument("-s", "--keep_smaller_than_MB",
         help="Limit to files larger than [size], in MB.")
 
     # return arguments values
@@ -169,18 +190,32 @@ def _processCli(args):
     """Process the CLI input.
     """
 
-    # print(args.type)
-    # print(args.time)
+    def select(type, **kwargs):
+        if type == 'all': cleanAll(**kwargs)
+        if type == 'tmp': cleanTmpDir(**kwargs)
+        if type == 'logs': cleanLogDir(**kwargs)
+        if type == 'drones': cleanDroneDirs(**kwargs)
 
-    if args.type == 'tmp':
-        cleanTmpDir(olderThanDate=args.date, olderThanDaysAgo=args.time, largerThanMB=args.size, confirm=True)
+    select(args.type, olderThanDate=args.keep_from_date, olderThanDaysAgo=args.keep_last_days, largerThanMB=args.keep_smaller_than_MB, confirm=True)
+
+    # if args.type == 'all':
+    #     cleanAll(olderThanDate=args.keep_from_date, olderThanDaysAgo=args.keep_last_days, largerThanMB=args.keep_smaller_than_MB, confirm=True)
+
+    # if args.type == 'tmp':
+    #     cleanTmpDir(olderThanDate=args.keep_from_date, olderThanDaysAgo=args.keep_last_days, largerThanMB=args.keep_smaller_than_MB, confirm=True)
+
+    # if args.type == 'logs':
+    #     cleanLogDir(olderThanDate=args.keep_from_date, olderThanDaysAgo=args.keep_last_days, largerThanMB=args.keep_smaller_than_MB, confirm=True)
+
+    # if args.type == 'drones':
+    #     cleanDroneDirs(olderThanDate=args.keep_from_date, olderThanDaysAgo=args.keep_last_days, largerThanMB=args.keep_smaller_than_MB, confirm=True)
 
 
 # ============================================================================ #
 # _isFileOlderThanDate
 def _isFileOlderThanDate(file_path, olderThanDate):
     """
-    olderThanDate: (str) YYYY-mm-dd"
+    olderThanDate: (str) YYYY-mm-dd. Note this will use last modified time.
     """
 
     # Parse the date string into a datetime object
