e1f20d4bad684da16b084b6abb0a1ca6cc6850dd
James Burgoyne <thejaburjunk+github@gmail.com>
2023-11-22 10:43:46 -0800

    docker image testing

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@
 # ============================================================================ #
 
 import os
-print(os.listdir())
+print(os.listdir('../'))
 
 from ..cfg import _cfg_queen as queen
 from ..cfg import _cfg_board as board
