54d3a6e779023edc7d0c53f9fe8cb62f78ab53ec
James Burgoyne <thejaburjunk+github@gmail.com>
2023-12-05 12:31:18 -0800

    debug testing

--- src/alcove_commands/alcove_base.py
+++ src/alcove_commands/alcove_base.py
@@ -54,7 +54,10 @@ def generateWaveDdr4(freq_list, amp_list, phi):
     X = np.zeros(lut_len,dtype='complex')
     #phi = np.random.uniform(-np.pi, np.pi, np.size(freq_list))
     print(freq_list)
-    print(k[0], phi[0], amp_list[0], X[k[0]])
+    print(k[0])
+    print(phi[0])
+    print(amp_list[0])
+    print(X[k[0]])
     for i in range(np.size(k)):
         X[k[i]] = np.exp(-1j*phi[i])*amp_list[i] # multiply by amplitude
     print("below")
