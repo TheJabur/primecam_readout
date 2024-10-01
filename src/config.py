fb664ce010204b97d4d0b196d29dba48091a38ae
James Burgoyne <thejaburjunk+github@gmail.com>
2024-10-01 14:50:02 -0700

    Fix missing file error in config.py

--- src/config.py
+++ src/config.py
@@ -25,9 +25,9 @@ sys.path.insert(1, parentDir(__file__))
 try:
     from cfg import _cfg_queen as queen
 except ImportError:
-    print("_cfg_queen.py is missing from cfg/ directory.")
+    print("Error: _cfg_queen.py is missing from cfg/ directory.")
 
 try:
     from cfg import _cfg_board as board
 except ImportError:
-    print("_cfg_board.py is missing from cfg/ directory.")
+    print("Error: _cfg_board.py is missing from cfg/ directory.")
