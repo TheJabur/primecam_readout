2a29938d5158bf6d0d051bac60e811943f93535e
James Burgoyne <thejaburjunk+github@gmail.com>
2025-07-02 13:31:30 -0700

    debug

--- src/queen.py
+++ src/queen.py
@@ -513,6 +513,7 @@ def _rectifyProcessBuffer(r):
     # set buffer size if needed
     if cfg.client_output_buffer_limit == 0:
         pubsub_buf_cfg = r.config_get('client-output-buffer-limit pubsub')
+        print(pubsub_buf_cfg)
         pubsub_buf_lim_soft = int((pubsub_buf_cfg.split())[1])
         cfg.client_output_buffer_limit = pubsub_buf_lim_soft
         print(f"Setting cfg.client_output_buffer_limit={cfg.client_output_buffer_limit}")
