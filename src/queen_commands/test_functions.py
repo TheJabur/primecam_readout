0abd595973b7b15a395e84dac40474605015694d
James Burgoyne <thejaburjunk+github@gmail.com>
2025-09-10 13:31:57 -0700

    debug

--- src/queen_commands/test_functions.py
+++ src/queen_commands/test_functions.py
@@ -176,12 +176,12 @@ def tls_array_test():
     # config
     nclo = 500 # MHz
     # steps_temp = [50, 75, 100, 125, 150, 175, 200, 300, 400, 500] # mK
-    steps_temp = [100, 150]
+    steps_temp = [50, 100]
     # steps_tone = [-5, -3, 0, 3, 5] # dB; Note not to exceed DAC max!
     # steps_tone = [-10, -8, -5, -2, 0] # dB
-    steps_tone = [-2, 0]
-    t_step = 2300 # s; time spent at each temperature step in total
-    t_stabilize = 1800 # s; time to wait for temp stabilization at each step
+    steps_tone = [-10, 0]
+    t_step = 4000 # s; time spent at each temperature step in total
+    t_stabilize = 3600 # s; time to wait for temp stabilization at each step
     t_tod = 60 # s; tod length at each step
     fs = 512e6/1024/1024 # samples per second (~488 Hz) (single drone)
     timestream = TimeStream(host="192.168.3.40", port=4096)
