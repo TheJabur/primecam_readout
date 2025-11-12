90df23a8da4f0cfe2d6ca96138a6b688e172270c
James Burgoyne <thejaburjunk+github@gmail.com>
2025-11-12 08:47:49 -0800

    debug

--- src/alcove_commands/__init__.py
+++ src/alcove_commands/__init__.py
@@ -1,31 +1,45 @@
-import os
-import sys
+from . import test_functions as test
+from . import board_utilities as utils
+from . import alcove_base
+from . import tones
+from . import sweeps
+from . import analysis
+from . import board_io
+from . import transceiver_serialdriver
 
-try:
-    gateware_version = int(os.environ.get('PRIMECAM_READOUT_GATEWARE_VERSION'))
-except:
-    gateware_version = 15 # default, mostly for control computer
-    
-if gateware_version >= 15:
-
-    from . import test_functions as test
-    from . import board_utilities as utils
-    from . import alcove_base_15 as alcove_base
-    from . import tones
-    from . import sweeps
-    from . import analysis
-    from . import board_io
-    from . import transceiver_serialdriver
-
-else:
-
-    from . import test_functions as test
-    from . import board_utilities as utils
-    from . import alcove_base_14 as alcove_base
-    from . import tones
-    from . import sweeps
-    from . import analysis
-    from . import board_io
-
-sys.modules['alcove_commands.alcove_base'] = alcove_base
 __all__ = ["test", "utils", "alcove_base", "tones", "sweeps", "analysis", "board_io", "transceiver_serialdriver"]
+
+
+
+
+# import os
+# import sys
+
+# try:
+#     gateware_version = int(os.environ.get('PRIMECAM_READOUT_GATEWARE_VERSION'))
+# except:
+#     gateware_version = 15 # default, mostly for control computer
+    
+# if gateware_version >= 15:
+
+#     from . import test_functions as test
+#     from . import board_utilities as utils
+#     from . import alcove_base_15 as alcove_base
+#     from . import tones
+#     from . import sweeps
+#     from . import analysis
+#     from . import board_io
+#     from . import transceiver_serialdriver
+
+# else:
+
+#     from . import test_functions as test
+#     from . import board_utilities as utils
+#     from . import alcove_base_14 as alcove_base
+#     from . import tones
+#     from . import sweeps
+#     from . import analysis
+#     from . import board_io
+
+# sys.modules['alcove_commands.alcove_base'] = alcove_base
+# __all__ = ["test", "utils", "alcove_base", "tones", "sweeps", "analysis", "board_io", "transceiver_serialdriver"]
---
+++ src/alcove_commands/alcove_base.py
@@ -0,0 +1,12 @@
+
+import os
+
+try:
+    gateware_version = int(os.environ.get('PRIMECAM_READOUT_GATEWARE_VERSION'))
+except:
+    gateware_version = 15 # default, mostly for control computer
+    
+if gateware_version >= 15:
+    from alcove_base_15 import *
+else:
+    from alcove_base_14 import *
