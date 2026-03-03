498d1c9f9365e5f978e0fddff9a49e8bdd705ca2
James Burgoyne <thejaburjunk+github@gmail.com>
2026-03-03 15:02:33 -0800

    debug

--- src/queen_commands/test_functions.py
+++ src/queen_commands/test_functions.py
@@ -158,8 +158,8 @@ def _waitForBlueforsTemperatureStable(
     t_start = time.time()
     while True:
 
-        print(f"temps={temps}")
-        print(f"times={times}")
+        # print(f"temps={temps}")
+        # print(f"times={times}")
 
         temps.append(controller.get_mxc_temperature())
         times.append(time.time())
@@ -275,7 +275,7 @@ def tls_array_test():
 
     i_T = i_P = 0
     msg = f"Running: (max {N_steps_T*t_stabilize_max} s; {N_packets_total} packets):"
-    # _progressBar(i_T*N_steps_P + i_P + 1, N_steps, msg)
+    _progressBar(i_T*N_steps_P + i_P + 1, N_steps, msg)
 
     status = controller.toggle_mxc_heater('on')
 
